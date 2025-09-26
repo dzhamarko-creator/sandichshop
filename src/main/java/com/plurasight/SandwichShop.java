2e9a8af386368c7f8ae1e1f47d817a0c19953b71
dzi-niska <dzhamarko@gmail.com>
2025-09-26 15:28:42 -0500

    Update SandwichShop.java

--- src/main/java/com/plurasight/SandwichShop.java
+++ src/main/java/com/plurasight/SandwichShop.java
@@ -1,17 +1,34 @@
-package com.plurasight;
+import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class GarlicSandwichShop {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Ask customer for sandwich size
+        System.out.println("Enter sandwich size (1 = Regular, 2 = Large): ");
+        int size = sc.nextInt();
+
+        double price;
+        if (size == 1) {
+            price = 5.45; // Regular
+        } else {
+            price = 8.95; // Large
         }
+
+        // Ask customer for their age
+        System.out.println("Enter your age: ");
+        int age = sc.nextInt();
+
+        // Apply  special shop discounts
+        if (age <= 17) {
+            price = price * 0.90; // 10% discount
+        } else if (age >= 65) {
+            price = price * 0.80; // 20% discount
+        }
+
+        // Show final garlic sandwich cost
+        System.out.println("Your sandwich will cost: $" + String.format("%.2f", price));
+
+        sc.close();
     }
 }
